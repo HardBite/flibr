b352045551260a9fea1103a134895e99c9379e46
Max <hardbitec0c0c0@gmail.com>
2014-12-26 02:43:18 +0200

    Beta version with python-populator

---
+++ create_and_populate.py
@@ -0,0 +1,34 @@
+import app.database
+import app.models
+import random as rnd
+
+eng = app.database.engine
+db_ses = app.database.db_session
+app.database.init_db()
+
+### generating some data for amusement and testing
+v1 = ["Let's ", "How to ", "Stop "]
+v2 = ["rock ", "draw ", "tell jokes ", "write a code "]
+v3 = ["hard", "good", "bad", "ugly", "well"]
+
+authors = ["Rockwell", "Drawgood", "Stopson", "Hardwell", "Letbe"]
+
+
+for author in authors:
+  a = app.models.Author()
+  a.name = author
+  a.save_or_error()
+  print a.name
+
+for n in range(25):
+  b = app.models.Book()
+  b.title = rnd.choice(v1)
+  for v in (v2, v3):
+    b.title += rnd.choice(v)
+  b.author = rnd.sample([1,2,3,4,5], rnd.randint(1, 5))
+  b.save_or_error()
+  print b.title, b.author
+
+
+
+
